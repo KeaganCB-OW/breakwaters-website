969631de8e853d95c4a8e2bbf5106685cd1026bb
KeaganCB-OW <160649882+KeaganCB-OW@users.noreply.github.com>
2025-09-22 23:30:06 +0200

    Add animated CardNav component and dashboard usage
    Introduces a new CardNav component with GSAP-powered animations and custom styling. Updates RODashboard to use CardNav with example navigation items. Adds GSAP as a dependency and downgrades TypeScript to 4.9.5 for compatibility. Also fixes import casing for SignUpPage in App.js.

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import RODashboard from './pages/RODashboard';
 import LoginPage from './pages/LoginPage';
-import SignupPage from './pages/SignupPage';
+import SignUpPage from './pages/SignUpPage';
 import { AuthProvider } from './context/AuthContext';
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
           <Route path="/dashboard" element={<RODashboard />} />
         </Routes>
       </Router>
